3b951c9e35d26935f5944d7a97aa4d7351bc9cee
Shane Runnels <shanerunnels@gmail.com>
2020-09-02 13:58:57 -0500

    got rid of develop folder

--- server.js
+++ server.js
@@ -10,4 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || )
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+});
