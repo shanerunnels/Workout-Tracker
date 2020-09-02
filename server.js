5c5fd4ea9105b0b47cc124cd6dc43db20b964c4e
Shane Runnels <shanerunnels@gmail.com>
2020-09-02 13:44:17 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || )
