e5732b51f5908caca7e333bc0e59a1ce60922466
Shane Runnels <shanerunnels@gmail.com>
2020-09-04 16:57:09 -0500

    changed server.js

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useUnifiedTopology: true,
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
